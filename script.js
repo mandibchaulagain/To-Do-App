27e2c43ba0888a90be22edb4f9c9c05104d57b3b
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-20 19:39:53 +0545

    completed the app

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const addBtn = document.querySelector("#add-btn");
-const newTaskInput = document.querySelector("#wrapper input");
-const taskContainer = document.querySelector("#tasks");
-const error = document.getElementById("error");
-const countValue = document.querySelector(".count-value");
-let taskCount = 0;
-
-const displayCount = (taskCount) => {
-    countValue.innerText = taskCount;
-};
-
-const addTask = () => {
-    const taskName = newTaskInput.value.trim();
-    error.style.display = "none";
-    if(!taskName){
-        setTimeout(()=>{
-            error.style.display = "block";
-        },200);
-        return;
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
     }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
 
-    const task = `<div class = "task">
-        <input type = "checkbox" class = "task-check">
-        <span class = "taskname">${taskName}</span>
-        <button class = "edit">
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
 
-        </button>
-        <button class = "delete">
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
 
-        </button>
-    </div>`;
-};
-
+        document.querySelector("#newtask input").value = "";
+    }
+}
